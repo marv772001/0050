f5a294639e38a467df574146b251688fcad00a83
Marvin Ueng <marv772001@hotmail.com>
2024-06-24 23:11:08 +0800

    add heatmap

--- stock.py
+++ stock.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime,timedelta
 import seaborn as sns
+from datetime import datetime,timedelta
+from pathlib import Path
+
 def loadCsv(path):
   df=pd.read_csv(path,encoding="utf8")
   return df
@@ -13,19 +15,35 @@ def process(df, origin):
             elif col!="日期":
               df.loc[:, col] = df[col].apply(lambda x: float(x.replace(',', '') if type(x) == str else x)).values
         return df
-def heatmap(df,start,end):
-    df4Correlation = DF_ALL.copy().drop(columns=['日期'])
+def heatmap(df):
+    df4Correlation = df.copy().drop(columns=['日期'])
     correlationDf = df4Correlation.corr()
     plt.figure(figsize=(25, 20))
     sns.heatmap(data=correlationDf, annot=True,fmt=".2f")
-    text=(f"Correlation Heatmap from {start} to {end}")
+    date=df["日期"]
+    startdate=date.iloc[0].strftime("%Y-%m-%d")
+    enddate=date.iloc[-1].strftime("%Y-%m-%d")
+    text=(f"Correlation Heatmap from {startdate} to {enddate}")
     plt.title(text)
-    savePath = "heatmap_{start}_{end}.png"
+    savePath =f"heatmap_{startdate}_{enddate}.png"
     plt.savefig(savePath)
     plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-
-
-            
+def splitbydate(df,num):
+    for i in range(0,10000,num):
+        start=i
+        end=i+num
+        final=57
+        if end<final:
+            newDF=df.iloc[start:end]
+            heatmap(newDF)
+    
+    for i in range(58,10000,num):
+        start=i
+        end=i+num
+        final=120
+        if end<final:
+            newDF=df.iloc[start:end]
+            heatmap(newDF)            
      
 DF1=loadCsv("0050漲跌幅.csv")
 DF2=loadCsv("0050 三大法人2023 4-6-1 .csv")
@@ -46,21 +64,12 @@ DF_ALL=pd.merge(DF1,DF2,on="日期")
 DF_ALL=pd.merge(DF_ALL,DF3,on="日期")
 DF_ALL=pd.merge(DF_ALL,DF4,on="日期")
 DF_ALL=pd.merge(DF_ALL,DF5,on="日期")
-DF_ALL.to_csv("final.csv",index=0)
+DF_ALL["大戶散戶"]=DF_ALL["成交量(張)"]/DF_ALL["成交筆數"]
+DF_ALL.drop(DF_ALL.columns[DF_ALL.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
+DF_ALL.to_csv("final.csv")
+splitbydate(DF_ALL,5)
+splitbydate(DF_ALL,7)
+splitbydate(DF_ALL,10)
 
-temp=DF_ALL["日期"]
-for i in temp[::5]:
-    start=i
-    end=i+timedelta(days=6)
-    final=datetime.strptime("2024-06-30","%Y-%m-%d")
-    if end<final:
-        newDF=DF_ALL[(DF_ALL["日期"]>=start)&(DF_ALL["日期"]<=end)]
-        heatmap(newDF,start,end)
 
-# new_DS.plot(x="收盤價",y="漲跌價",kind="scatter")
-# plt.rcParams['font.sans-serif'] = ['Microsoft YaHei'] # 使用中文字體
 
-# for i,j in zip(DS["交易日期"],DS["收盤價"]):
-#   if i>='2023/3/1' and i<'2023/3/10':
-#      list_data.append(j)
-# print(list_data)    
